343e62464c812751d727b1fbe2fef7f050de61a5
unknown <francisco.falcon_13@hotmail.com>
2021-05-17 23:03:58 -0300

    mmmm

--- files/install.js
+++ files/install.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let deferredInstallPrompt = null;
+let deferredInstallPrompt;
 const installButton = document.getElementById("instalbut");
 installButton.addEventListener('click', installPWA);
 
@@ -8,12 +8,10 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 function saveBeforeInstallPromptEvent(evt){
     deferredInstallPrompt= evt;
-    installButton.removeAttribute('hidden');
 }
 
 function installPWA(evt){
     deferredInstallPrompt.prompt();
-    evt.srcElement.setAtribute('hidden', true);
     deferredInstallPrompt.userChoice.then((choice)=>{
         if(choice.outcome === "accepted"){
             console.log("aceptado")
