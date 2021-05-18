6e3790780cf203b31278e48c47dc28a85fe09061
unknown <francisco.falcon_13@hotmail.com>
2021-05-17 21:01:48 -0300

    probando boton

---
+++ files/install.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById("install");
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+function saveBeforeInstallPromptEvent(evt){
+    deferredInstallPrompt= evt;
+    installButton.removeAttribute('hidden');
+}
+
+function installPWA(evt){
+    deferredInstallPrompt.prompt();
+    evt.srcElement.setAtribute('hidden', true);
+    deferredInstallPrompt.userChoice.then((choice)=>{
+        if(choice.outcome === "accepted"){
+            console.log("aceptado")
+        }else{
+            console.log("no aceptado")
+        }
+        deferredInstallPrompt = null;
+}) 
+ 
+}
